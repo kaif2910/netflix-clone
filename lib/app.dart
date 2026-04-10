8b7068f90e84df5217e9d902687e3ea58a078f29
kaif2910 <kaif282907@gmail.com>
2026-04-11 00:28:21 +0530

    fix: restore library/part structure for compilation

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,5 @@
+library marvel_cinema;
+
 // Dart Imports
 import 'dart:async';
 import 'dart:convert';
@@ -12,40 +14,48 @@ import 'package:shared_preferences/shared_preferences.dart';
 // Plugins import...
 import 'package:fluro/fluro.dart';
 import 'package:video_player/video_player.dart';
-import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
-// Internal Imports
-import 'src/helpers/config/constants.dart';
-import 'src/helpers/config/application.dart';
-import 'src/helpers/config/routes.dart';
-import 'src/helpers/config/route_handlers.dart';
-import 'src/models/episode.dart';
-import 'src/models/result.dart';
-import 'src/models/item_model.dart';
-import 'src/blocs/movies_bloc.dart';
-import 'src/resources/movie_api_provider.dart';
-import 'src/resources/repository.dart';
-import 'src/utils/theme/color.dart';
-import 'src/utils/theme/typography.dart';
-import 'src/utils/local_store.dart';
-import 'src/pages/video/index.dart';
-import 'src/pages/video/state.dart';
-import 'src/pages/home/index.dart';
-import 'src/pages/home/state.dart';
-import 'src/pages/summary/index.dart';
-import 'src/pages/summary/state.dart';
-import 'src/pages/filter/index.dart';
-import 'src/pages/filter/state.dart';
-import 'src/pages/detail/index.dart';
-import 'src/pages/detail/state.dart';
-import 'src/pages/profile/index.dart';
-import 'src/pages/profile/state.dart';
-import 'src/widgets/tvshow-list/index.dart';
-import 'src/widgets/media_image.dart';
-import 'src/widgets/player-life-cycle/index.dart';
-import 'src/widgets/player-life-cycle/state.dart';
-import 'src/widgets/player-controls/index.dart';
-import 'src/widgets/player-controls/state.dart';
+// Router
+part 'src/helpers/config/constants.dart';
+part 'src/helpers/config/application.dart';
+part 'src/helpers/config/routes.dart';
+part 'src/helpers/config/route_handlers.dart';
+
+// Models
+part 'src/models/episode.dart';
+part 'src/models/result.dart';
+part 'src/models/item_model.dart';
+
+// Blocs
+part 'src/blocs/movies_bloc.dart';
+
+// Resources
+part 'src/resources/movie_api_provider.dart';
+part 'src/resources/repository.dart';
+
+part 'src/utils/theme/color.dart';
+part 'src/utils/theme/typography.dart';
+part 'src/utils/local_store.dart';
+part 'src/pages/video/index.dart';
+part 'src/pages/video/state.dart';
+part 'src/pages/home/index.dart';
+part 'src/pages/home/state.dart';
+part 'src/pages/summary/index.dart';
+part 'src/pages/summary/state.dart';
+part 'src/pages/filter/index.dart';
+part 'src/pages/filter/state.dart';
+part 'src/pages/detail/index.dart';
+part 'src/pages/detail/state.dart';
+part 'src/pages/profile/index.dart';
+part 'src/pages/profile/state.dart';
+
+// Widgets
+part 'src/widgets/tvshow-list/index.dart';
+part 'src/widgets/media_image.dart';
+part 'src/widgets/player-life-cycle/index.dart';
+part 'src/widgets/player-life-cycle/state.dart';
+part 'src/widgets/player-controls/index.dart';
+part 'src/widgets/player-controls/state.dart';
 
 class MarvelCinema extends StatelessWidget {
   MarvelCinema({Key? key}) : super(key: key) {
